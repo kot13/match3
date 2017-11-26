2257de8f36b6e5cb29eb763178493b134844382e
Павел Муркин <kot_tdf@mail.ru>
2017-11-26 09:59:39 +0300

    add hurt

--- www/assets/js/game/splash.js
+++ www/assets/js/game/splash.js
@@ -39,15 +39,17 @@ class Splash {
         pgame.load.image('cat_ginger','assets/images/cat2.png');
         pgame.load.image('cat_lock','assets/images/cat-lock.png');
 
-        pgame.load.spritesheet('cat_gray_stay', '/assets/images/sprites/cat-gray/stay.png', 273, 237, 10);
+        pgame.load.spritesheet('cat_gray_stay', '/assets/images/sprites/cat-gray/stay.png', 273, 237, 17);
         pgame.load.spritesheet('cat_gray_klubok', '/assets/images/sprites/cat-gray/klubok.png', 277, 237, 3);
         pgame.load.spritesheet('cat_gray_lay', '/assets/images/sprites/cat-gray/lay.png', 273, 237, 10);
-        pgame.load.spritesheet('cat_gray_die', '/assets/images/sprites/cat-gray/die.png', 280, 237, 10);
+        pgame.load.spritesheet('cat_gray_die', '/assets/images/sprites/cat-gray/die.png', 280, 237, 9);
+        pgame.load.spritesheet('cat_gray_hurt', '/assets/images/sprites/cat-gray/hurt.png', 280, 237, 9);
 
         pgame.load.spritesheet('cat_ginger_stay', '/assets/images/sprites/cat-ginger/stay.png', 273, 237, 17);
         pgame.load.spritesheet('cat_ginger_klubok', '/assets/images/sprites/cat-ginger/klubok.png', 277, 237, 3);
         pgame.load.spritesheet('cat_ginger_lay', '/assets/images/sprites/cat-ginger/lay.png', 273, 237, 10);
-        pgame.load.spritesheet('cat_ginger_die', '/assets/images/sprites/cat-ginger/die.png', 280, 237, 10);
+        pgame.load.spritesheet('cat_ginger_die', '/assets/images/sprites/cat-ginger/die.png', 280, 237, 9);
+        pgame.load.spritesheet('cat_ginger_hurt', '/assets/images/sprites/cat-ginger/hurt.png', 280, 237, 9);
     }
 
     create() {
